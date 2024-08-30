2b3d4d67cfab486b1f6861be981750a82f94a170
mjlyons7 <mjlyons19@gmail.com>
2024-08-30 10:47:09 -0700

    Updated bullet physics - now calculates how far it needs to move that frame, then travels to the the next Deflector surface along its path and reflects until it has used all its movement. Added debug menu to TestRoom2.

--- Bullet.cs
+++ Bullet.cs
@@ -1,103 +1,159 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
 public partial class Bullet : Area2D
 {
-	float speed = 1000; // pixels per second
-	float rayLength = 25;
-	float lifespan = 5; // seconds
-	Vector2 direction;
-	DebugHelper helper;
+    float speed = 1000; // pixels per second
+    float lifespan = 5; // seconds
+    float distanceToTravel; // per frame
+    Vector2 direction;
 
-	int collisionCount;
-	string previousAreaCollidedWith;
+    Sprite2D raySprite;
+    DebugHelper helper;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+    int collisionCount;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
         direction = new Vector2((float)Math.Cos(Rotation), (float)Math.Sin(Rotation));
 
-		// draw ray, for debugging
-		var raySprite = GetNode<Sprite2D>("ray");
-		if (Globals.DEBUG_ON)
-        {
-			raySprite.Scale = new Vector2(rayLength, 2);
-			raySprite.Position = new Vector2(rayLength / 2, 0);
-			lifespan = 60;
-        }
-        else
-        {
-			raySprite.Visible = false;
-        }
+        // draw ray, for debugging
+        raySprite = GetNode<Sprite2D>("ray");
 
-		helper = new DebugHelper();
+        if (Globals.DEBUG_ON)
+            lifespan = 60;
+
+        helper = new DebugHelper();
     }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		int speedModifier = 1;
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        int speedModifier = 1;
 
-		// low framerate mode, debug menu
-		if (Globals.DEBUG_ON)
-			if (!Input.IsActionJustPressed("debug_advance"))
-				speedModifier = 0;
+        // low framerate mode, debug menu
+        if (Globals.DEBUG_ON)
+            if (!Input.IsActionJustPressed("debug_advance"))
+                speedModifier = 0;
 
         // x = x0 + vt
-        Position += ((float)delta * speed * speedModifier) * direction;
+        distanceToTravel += ((float)delta * speed * speedModifier);
 
-		helper.Run(delta);
+        //animation
+        if (Globals.DEBUG_ON)
+        {
+            if (distanceToTravel > 0)
+            {
+                raySprite.Visible = true;
+                raySprite.Scale = new Vector2(distanceToTravel, 2);
+                raySprite.Position = new Vector2(20 / 2, 0);
+            }
+        }
 
-		// kill
-		if (lifespan < helper.totalSecondsPassed)
-			QueueFree();
-	}
+        // kill
+        if (lifespan < helper.totalSecondsPassed)
+        QueueFree();
+
+
+        // logging
+        var strings = new List<string>();
+        //strings.Add("Distance to travel: " + distanceToTravel.ToString());
+        //helper.PrintAfterInterval(strings);
+        helper.Run(delta);
+    }
 
     public override void _PhysicsProcess(double delta)
     {
 
-		// detect collision surface with ray trace
-		// TODO: only get results for reflectors
-		var spaceState = GetWorld2D().DirectSpaceState;
-		var from = this.Position;
-		var to = this.Position + (direction.Normalized() * rayLength);
-		var query = PhysicsRayQueryParameters2D.Create(from, to);
-		var result = spaceState.IntersectRay(query);
-		if (result.Count > 0)
+        // movement
+        int i = 0;
+        int maxCollisionsThisFrame = 10;
+        while(distanceToTravel > 0 && i < maxCollisionsThisFrame)
         {
-			StringName reflectorGroupName = new StringName(Globals.GROUP_REFLECTORS);
+            CheckForCollisions();
+            i++;
+        }
+
+        if (i == maxCollisionsThisFrame)
+        {
+            var strings = new List<string>()
+                {
+                    "Hit max collision count"
+                };
+            helper.PrintAfterInterval(strings);
+            distanceToTravel = 0;
+        }
+
+    }
+
+    private void CheckForCollisions()
+    {
+        // detect collision surface with ray trace
+        var spaceState = GetWorld2D().DirectSpaceState;
+        var from = Position;
+        var to = Position + (direction.Normalized() * distanceToTravel);
+        var query = PhysicsRayQueryParameters2D.Create(from, to);
+        var result = spaceState.IntersectRay(query);
+        if (result.Count > 0)
+        {
+            StringName reflectorGroupName = new StringName(Globals.GROUP_REFLECTORS);
 
             var collidedNode = result["collider"].As<Node>();
 
             if (collidedNode is not null)
-			{
-				var collidedGroupNames = collidedNode.GetGroups();
+            {
+                var collidedGroupNames = collidedNode.GetGroups();
 
-				// decide what to do based on object collided with. Default case is despawn
-				if (collidedGroupNames.Contains(reflectorGroupName))
-					ReflectOff(result);
-				else if (collidedNode is IMortal mortal)
-				{
-					// TODO: should be pub/sub model, I think
-					mortal.OnHit();
+                // decide what to do based on object collided with. Default case is despawn
+                if (collidedGroupNames.Contains(reflectorGroupName))
+                    MoveToAndReflectOff(result);
+                else if (collidedNode is IMortal mortal)
+                {
+                    // move up to object, but keep travelling afterwards
+                    Position += distanceToTravel * direction;
+                    mortal.OnHit();
                 }
-				else
-					QueueFree();
-			}
+                else
+                {
+                    Position += distanceToTravel * direction;
+                    distanceToTravel = 0;
+                    QueueFree();
+                }
+            }
         }
-	}
+        // no collision
+        else
+        {
+            // TODO: shouldn't have this code 3 times...
+            Position += distanceToTravel * direction;
+            distanceToTravel = 0;
+        }
+    }
 
     // reflect off a surface
-    private void ReflectOff(Godot.Collections.Dictionary collisionResult)
+    private void MoveToAndReflectOff(Godot.Collections.Dictionary collisionResult)
     {
-		collisionCount += 1;
+        collisionCount += 1;
+        float moveTolerance = 0.001f; // Don't want to move right up to surface, because after reflection might be stuck inside it
 
-		var colliderNormal = (Vector2)collisionResult["normal"];
-		var colliderAngle = Math.Atan2(colliderNormal.Y, colliderNormal.X);
+        // just for debugging
+        var colliderId = (string)collisionResult["collider_id"];
 
-		Rotation = (float)(Math.PI - Rotation) + (float)(2 * colliderAngle);
-		direction = new Vector2((float)Math.Cos(Rotation), (float)Math.Sin(Rotation));
-	}
+        // move up to the surface
+        var distanceToCollision = (Position - (Vector2)collisionResult["position"]).Length();
+        Debug.Assert(distanceToCollision <= distanceToTravel);
+        Position += ((distanceToCollision - moveTolerance) * direction);
+        distanceToTravel -= distanceToCollision;
+
+        // reflect off
+        var colliderNormal = (Vector2)collisionResult["normal"];
+        var colliderAngle = Math.Atan2(colliderNormal.Y, colliderNormal.X);
+
+        Rotation = (float)(Math.PI - Rotation) + (float)(2 * colliderAngle);
+        direction = new Vector2((float)Math.Cos(Rotation), (float)Math.Sin(Rotation));
+    }
 }
